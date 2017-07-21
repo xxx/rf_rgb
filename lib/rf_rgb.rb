4f2cf3b634bb938d89c1e7923d05c8f61417af21
mpd <mpd@jesters-court.net>
2017-07-20 19:20:32 -0700

    actually verify acks received from the board, and allow for a couple of retries in the case it doesn't match.

--- lib/rf_rgb.rb
+++ lib/rf_rgb.rb
@@ -4,6 +4,8 @@ require 'rf_rgb/protocol'
 require 'rf_rgb/version'
 
 module RfRgb
+  class MismatchedVerificationError < StandardError; end
+
   # 0 is silent, 4 is spewy.
   LIBUSB_DEBUG = ENV['LIBUSB_DEBUG'] || 3
 end
--- lib/rf_rgb/keyboard.rb
+++ lib/rf_rgb/keyboard.rb
@@ -34,8 +34,7 @@ module RfRgb
     end
 
     def save
-      send_message RfRgb::Protocol.save_changes
-      receive_message
+      send_and_verify RfRgb::Protocol.save_changes
     end
 
     def release_to_os
@@ -46,8 +45,7 @@ module RfRgb
 
     def disable_effect
       reset_effect
-      send_message RfRgb::Protocol.disable_effects
-      receive_message
+      send_and_verify RfRgb::Protocol.disable_effects
     end
 
     def colors=(new_colors)
@@ -58,84 +56,69 @@ module RfRgb
       calls = RfRgb::Protocol.user_specified_colors(new_colors)
       reset_effect
       calls.each do |call|
-        send_message call
-        ack = receive_message
-        # verify via ack[0..2] == "\x55\x55#{call[2]}" or something
+        send_and_verify call
       end
     end
 
     def effect_rainbow_wave
       reset_effect
-      send_message RfRgb::Protocol.rainbow_wave
-      receive_message
+      send_and_verify RfRgb::Protocol.rainbow_wave
     end
 
     def effect_pressed_key(rgb_hex)
       reset_effect
-      send_message RfRgb::Protocol.pressed_key_lighting(rgb_hex)
-      receive_message
+      send_and_verify RfRgb::Protocol.pressed_key_lighting(rgb_hex)
     end
 
     def effect_pressed_key_with_backlight(rgb_hex)
       reset_effect
-      send_message RfRgb::Protocol.pressed_key_lighting_with_backlight(rgb_hex)
-      receive_message
+      send_and_verify RfRgb::Protocol.pressed_key_lighting_with_backlight(rgb_hex)
     end
 
     def effect_shooting_star(rgb_hex, interval)
       reset_effect
-      send_message RfRgb::Protocol.shooting_star(rgb_hex, interval)
-      receive_message
+      send_and_verify RfRgb::Protocol.shooting_star(rgb_hex, interval)
     end
 
     def effect_demo_mode
       reset_effect
-      send_message RfRgb::Protocol.demo_mode
-      receive_message
+      send_and_verify RfRgb::Protocol.demo_mode
     end
 
     def effect_random_lights
       reset_effect
-      send_message RfRgb::Protocol.random_lights
-      receive_message
+      send_and_verify RfRgb::Protocol.random_lights
     end
 
     def effect_color_bar
       reset_effect
-      send_message RfRgb::Protocol.color_bar
-      receive_message
+      send_and_verify RfRgb::Protocol.color_bar
     end
 
     # There's more to key locking than just this, but the way it's done is odd
     # and is among the lowest of priorities on this project.
     def lock_keys
-      send_message RfRgb::Protocol.key_lock
-      receive_message
+      send_and_verify RfRgb::Protocol.key_lock
     end
 
     def unlock_keys
-      send_message RfRgb::Protocol.key_unlock
-      receive_message
+      send_and_verify RfRgb::Protocol.key_unlock
     end
 
     def brightness=(new_brightness)
-      send_message RfRgb::Protocol.change_brightness(new_brightness)
-      receive_message
+      send_and_verify RfRgb::Protocol.change_brightness(new_brightness)
     end
 
     def actuation_height=(new_height)
-      send_message RfRgb::Protocol.change_actuation_height_all(new_height)
-      receive_message
+      send_and_verify RfRgb::Protocol.change_actuation_height_all(new_height)
     end
 
     def swap_caps_ctrl
-      send_message RfRgb::Protocol.swap_caps_ctrl
-      receive_message
+      send_and_verify RfRgb::Protocol.swap_caps_ctrl
     end
 
     def unswap_caps_ctrl
-      send_message RfRgb::Protocol.unswap_caps_ctrl
-      receive_message
+      send_and_verify RfRgb::Protocol.unswap_caps_ctrl
     end
 
     private
@@ -143,8 +126,7 @@ module RfRgb
     # I don't know what this actually does, but the Windows software
     # sends this message before any effect change.
     def reset_effect
-      send_message RfRgb::Protocol.reset_effect
-      receive_message
+      send_and_verify RfRgb::Protocol.reset_effect
     end
 
     def initialize_device(device)
@@ -170,6 +152,29 @@ module RfRgb
         dataIn: 64,
         timeout: 10_000
       )
+    end
+
+    def send_and_verify(data, retries = 2)
+      retry_count = 0
+      begin
+        send_message data
+        ack = receive_message
+        verify_message!(data, ack)
+      rescue RfRgb::MismatchedVerificationError
+        retry_count += 1
+        retry if retry_count <= retries
+      end
+    end
+
+    # The keyboard acknowledges each packet sent with a response of \x55\x55\x<third byte of sent packet>
+    def verify_message(data, ack)
+      ack.bytes[0..2] == "\x55\x55#{data[2]}".bytes
+    end
+
+    def verify_message!(data, ack)
+      unless verify_message(data, ack)
+        raise RfRgb::MismatchedVerificationError.new("Ack[#{ack.bytes}] did not match data[#{data.bytes}]!")
+      end
     end
   end
 end
