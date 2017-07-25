0133cf0393f9cd88a04b18d90832a6477eb8919f
mpd <mpd@jesters-court.net>
2017-07-25 00:41:11 -0700

    brightness high for my kb

--- examples/mpd_keyboard.rb
+++ examples/mpd_keyboard.rb
@@ -4,5 +4,6 @@ require 'rf_rgb'
 RfRgb::Keyboard.run_and_release do |keyboard|
   keyboard.effect_pressed_key '00ffff'
   keyboard.actuation_height = RfRgb::Protocol::HEIGHT_15
+  keyboard.brightness = RfRgb::Protocol::BRIGHTNESS_HIGH
   keyboard.save
 end
